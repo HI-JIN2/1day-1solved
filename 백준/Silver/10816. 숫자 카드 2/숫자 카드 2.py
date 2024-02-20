dc08507d5c2f127a9d80b86fd41c77cde39010d4
Jin Yu <jini1514@soongsil.ac.kr>
2024-02-20 14:49:25 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 844 ms, Memory: 115672 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+n=int(input())
+n_array= list(map(int,input().split()))
+
+m=int(input())
+m_array=list(map(int,input().split()))
+
+count = Counter(n_array)
+
+for i in m_array:
+    if i in count:
+        print(count[i], end = " ")
+    else: 
+        print(0, end=" ")
