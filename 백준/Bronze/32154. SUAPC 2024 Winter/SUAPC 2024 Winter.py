f01050a33fca975606e6d879475b5fe922696ed1
유진 <qldls0307@naver.com>
2024-12-02 10:58:54 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,22 @@
+n=int(input())
+
+if(n==1):
+    print("11\nA B C D E F G H J L M")
+if(n==2):
+    print("9\nA C E F G H I L M")
+if(n==3):
+    print("9\nA C E F G H I L M")  
+if(n==4):
+    print("9\nA B C E F G H L M")  
+if(n==5):
+    print("8\nA C E F G H L M")
+if(n==6):
+    print("8\nA C E F G H L M")
+if(n==7):
+    print("8\nA C E F G H L M")  
+if(n==8):
+    print("8\nA C E F G H L M")     
+if(n==9):
+    print("8\nA C E F G H L M")  
+if(n==10):
+    print("8\nA B C F G H L M")   
