e0528d5bd6b38e4d1b26ff04b33cd8fdc824e3e3
유진 <qldls0307@naver.com>
2025-10-29 17:41:15 +0900

    [Bronze II] Title: 블랙잭, Time: 80 ms, Memory: 38784 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,12 +1,18 @@
-N, M = map(int, input().split())
-lst = list(map(int, input().split()))
-nlst = []
-for i in range(N):
-    for j in range(i+1, N):
-        for k in range(j+1, N):
-            three =  lst[i] + lst[j] + lst[k]
-            if three > M:
+#m을 넘지 않으면서 가장 가깝게
+
+n,m = map(int,input().split())
+card = list(map(int,input().split()))
+card.sort(reverse=True)
+
+hap_list = []
+
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            hap = card[i] + card[j]+ card[k]
+            if hap > m:
                 continue
             else:
-                nlst.append(three)
-print(max(nlst))
+                hap_list.append(hap)
+
+print(max(hap_list))
