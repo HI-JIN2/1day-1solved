5049ebcb0a6a8a38c418f28b9d7aa2cdbd105f25
Jin Yu <qldls0307@naver.com>
2026-03-21 18:41:48 +0900

    [Bronze III] Title: 윷놀이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,13 @@
+for i in range(3):
+    data = list(map(int,input().split()))
+
+    if data.count(0) == 1:
+        print("A")
+    elif data.count(0) == 2:
+        print("B")
+    elif data.count(0) == 3:
+        print("C")
+    elif data.count(0) == 4:
+        print("D")
+    else:
+        print("E")
