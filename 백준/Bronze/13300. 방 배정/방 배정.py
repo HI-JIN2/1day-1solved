b2e8538f91aa36d0eb40a5bca3f9a8e82ac27343
Yu Jin <qldls0307@naver.com>
2026-04-04 20:46:51 +0900

    [Bronze II] Title: 방 배정, Time: 32 ms, Memory: 34536 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,23 @@
+import math
+
+n, k =map(int,input().split())
+
+fdata = [0]*6
+data = [0]*6
+answer = 0
+for i in range(n):
+    s,y = map(int,input().split())
+    if s == 0:
+        fdata[y-1]+=1
+    else:
+        data[y-1]+=1
+
+# print(data,fdata)
+
+for d in fdata:
+    answer += math.ceil(d/2)
+
+for d in data:
+    answer += math.ceil(d/2)
+
+print(answer)
