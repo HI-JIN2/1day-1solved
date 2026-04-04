a743dee03500a5dcc40d4a1a285073b5896196ec
Yu Jin <jini1514@soongsil.ac.kr>
2026-04-04 20:38:27 +0900

    Update 쇠막대기.py

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -3,13 +3,13 @@ stack = []
 answer = 0
 
 for i in range(len(data)):
-    if data[i] == '(':
+    if data[i] == '(': #한번에 잘리는 막대기의 수를 더해나감
         stack.append('(')
     else:  # ')'
         stack.pop()
         if data[i-1] == '(':   # 바로 앞이 '('면 레이저
-            answer += len(stack)
+            answer += len(stack) #현재 스택에 있는 수 만큼 한번에 잘렸음
         else:                  # 쇠막대기 끝
-            answer += 1
+            answer += 1 #쇠막대기 끝났으니까 하나 더해 
 
 print(answer)
