8e95ba5375248f05a77bf0dee7a4ff80670ca9e9
Yu Jin <qldls0307@naver.com>
2026-04-06 10:41:41 +0900

    [Gold V] Title: 회의실 배정, Time: 2716 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,20 +1,17 @@
 n = int(input())
-data = []
-answer = 0
-endtime = 0 #현재 잡힌 회의가 끝나는 시간
+data = [ list(map(int,input().split())) for _ in range(n)]
 
-for _ in range(n):
-    start, end = map(int,input().split())
-    data.append((start,end))
 data.sort(key = lambda x: (x[1], x[0]))
-# print(data)
 
+# print(data)
+answer = 0
+
+now = 0
 for t in data:
     start, end = t
 
-    if start >= endtime: #뉴 타깃이 현재 잡힌 회의가 끝나고 나서 시작하면
-        #끝나자마자 다른 회의 시작할 수 있으니까 등호
-        answer +=1 # 개수 더하고
-        endtime = end #현재 잡힌 회의가 끝날 시간을 바꿔줌
+    if now <=start:
+        answer+=1
+        now = end
 
 print(answer)
