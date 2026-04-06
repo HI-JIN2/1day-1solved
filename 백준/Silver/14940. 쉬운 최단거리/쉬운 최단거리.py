942d4cdd3af9d4d1b3ffa25d5d0b0def909de3b4
Yu Jin <qldls0307@naver.com>
2026-04-06 10:42:05 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 444 ms, Memory: 41664 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,44 +1,45 @@
+import sys
 from collections import deque
 
-n, m =map(int,input().split())
+input = sys.stdin.readline
 
-data = [  list(map(int,input().split()))  for _ in range(n)]
+n, m = map(int,input().split())
 
+data = [ list(map(int,input().split())) for _ in range(n)]
 # print(data)
-
-# 상 하 좌 우
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
-
+dist = [[-1]*m for _ in range(n)] #기본 값 -1
 
 def bfs(r,c):
-    q = deque()
+    q= deque()
     q.append((r,c))
+    dist[r][c] = 0 #애초에 목표지점인 2에서 호출을 하니까
 
     while q:
         r,c = q.popleft()
-        for i in range(4):
-            nr = dr[i]+r
-            nc = dc[i]+c
 
-            if nr < 0 or nr >= n or nc < 0 or nc >= m:
+        for dr, dc in ( [0,1], [1,0], [0,-1], [-1,0]):
+            nr= r+dr
+            nc = c+dc
+
+            if 0>nr or nr>=n or 0>nc or nc>=m:
                 continue
-            if data[nr][nc] == 0:
-                continue
-            if data[nr][nc] == 1 and dist[nr][nc] == -1: #갈 수 있고, 방문 안한곳만
+            elif data[nr][nc] == 1 and dist[nr][nc]==-1: #그 범위여야만 & 갈 수 있어야만 & 방문을 안했어야만
                 q.append((nr,nc))
-                dist[nr][nc] = dist[r][c]+1
+                dist[nr][nc] = dist[r][c]+1 # 선방문 처리
+            else:
+                continue
 
-dist = [ [ -1 ]*m for _ in range(n)]
-# visited = [[False]*m for _ in range(n)]
+
+
+# print(dist)
 
 for r in range(n):
     for c in range(m):
-        if data[r][c] == 2:# 목표 지점
-            dist[r][c] = 0
+        if data[r][c] == 2:
             bfs(r,c)
-        elif data[r][c] == 0: #원래 0이면 0임
+            dist[r][c] = 0
+        elif data[r][c] == 0: #원래 갈 수 없으면 0
             dist[r][c] = 0
 
 for r in range(n):
-    print(*dist[r] ,end="\n")
+    print(*dist[r])
