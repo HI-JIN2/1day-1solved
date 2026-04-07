5106a86525b7b25413b0f12a8d61c38573d0cc9a
Yu Jin <qldls0307@naver.com>
2026-04-07 09:44:17 +0900

    [level 3] Title: 네트워크, Time: 1.05 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,25 +1,22 @@
-from collections import deque
-
 def solution(n, computers):
     answer = 0
     
-    visited = [False] * (n+1)
     
-    def bfs(n, computers, start, visited):
-        queue = deque()
-        queue.append(start)
-
-        while queue:
-            x = queue.popleft()
-            visited[x] = True
-
-            for j in range(n):  #012
-                if j!=n and computers[x][j] == 1:
-                    if visited[j] == False:
-                        queue.append(j)
-
-    for com in range(n):
-        if visited[com] == False:
-            bfs(n,computers, com, visited)
-            answer+=1
+    def dfs(i):
+        visited[i]= True #방문처리
+        for k in range(n): #노드 
+            if computers[i][k] == 1 and visited[k] == False: #연결되어 있고 방문 안했어야
+                # print(k)
+                dfs(k) #방문             
+        
+        
+    visited=[False]*n 
+    
+    for i in range(n):
+        if visited[i] == False:
+            # print("시작",i)
+            answer+=1 #연결되어 있다면 여기서 다시 시작하지 않음 
+            #연결되어 있다면 Dfs에서 돌았었을 것임
+            dfs(i)
+    
     return answer
