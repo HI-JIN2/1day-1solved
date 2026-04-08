3f2c9dff35da9aa9126a9d1c20bf0ae4224e7219
Yu Jin <qldls0307@naver.com>
2026-04-08 15:25:16 +0900

    [level 2] Title: 타겟 넘버, Time: 599.09 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,23 @@
+from collections import deque
+
 def solution(numbers, target):
-    answer = 0
-    return answer
+    
+    
+    
+    def bfs(index,total): #큐에 먼저 넣고 방문처리
+        answer = 0
+        q = deque()
+        q.append((index,total))
+        
+        while q:
+            i, t = q.popleft()
+            if i == len(numbers):
+                if t ==target:
+                    answer+=1
+            else:
+                q.append((i+1, t+numbers[i]))
+                q.append((i+1, t-numbers[i]))
+        return answer
+        
+            
+    return bfs(0,0)
