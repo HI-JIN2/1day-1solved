01ae26b5288f3dbdbb79bc554f94ee3acb7df57b
Yu Jin <jini1514@soongsil.ac.kr>
2026-04-08 16:18:11 +0900

    Update 숫자 짝꿍.py

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,3 +1,37 @@
 def solution(X, Y):
-    answer = ''
-    return answer
+
+    l = []
+    
+    X = list(map(int,X))
+#     X.sort()
+    
+    Y = list(map(int,Y))
+#     Y.sort()
+    
+    #공통의 정수를 뽑아내야함
+    # for x in X:
+        # if x in Y: # in이 비싸다 
+            # l.append(x)
+            # Y.remove(x)
+            
+    xl = [0]*10
+    yl = [0]*10
+    for x in X:
+        xl[x]+=1
+    for y in Y:
+        yl[y]+=1
+    # print(xl,yl)
+        
+    for i in range(10):
+        cnt = min(xl[i],yl[i])
+        for _ in range(cnt):
+            l.append(i)
+            
+    l.sort(reverse=True)
+    # print(l)
+    
+    if len(l)==0:
+        return "-1"
+    elif l[0] == 0:
+        return "0"
+    return "".join(list(map(str,l)))
