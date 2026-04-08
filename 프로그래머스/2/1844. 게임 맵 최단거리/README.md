3d26d8b3a8e07a50f21dbf7b6edd3f4197727f47
Yu Jin <qldls0307@naver.com>
2026-04-08 15:52:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.82 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 9.26 MB, 시간: 9.34 ms
+메모리: 9.35 MB, 시간: 9.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 06일 15:33:02
+2026년 04월 08일 15:52:11
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,39 +1,31 @@
 from collections import deque
 
-
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
+    answer = 0
+    n = len(maps) #세로
+    m = len(maps[0]) #가로
     
-    dist = [ [-1]*m for _ in range(n)]
-
     def bfs(r,c):
-        #출발지인 0,0에서 부터 닿을 수 있는 모든 지점까지의 거리를 구할 것임
         q = deque()
         q.append((r,c))
-        dist[r][c] = 0 #출발지에서 출발지까지의 거리는 0 
-
+        dist[r][c]=1
+        
         while q:
             r,c = q.popleft()
-
-            for dr,dc in ((1,0), (-1,0), (0,1),(0,-1)):
+            
+            for dr, dc in ((0,1), (0,-1),(1,0),(-1,0)):
                 nr = dr+r
                 nc = dc+c
-
-                if 0>nr or nr>=n or 0>nc or nc>=m:
-                    continue #범위 아웃
-
-                elif dist[nr][nc] == -1 and maps[nr][nc] == 1: #안가야하고 갈 수 있는 곳만
+                
+                if 0>nr or nr>=n or 0>nc or nc>=m: #범위 제한
+                    continue
+                
+                elif maps[nr][nc] == 1 and dist[nr][nc] ==-1: # 갈 수 있어야하며 안갔어야함
+                    dist[nr][nc] = dist[r][c]+maps[nr][nc] #이전까지의 거리+1
                     q.append((nr,nc))
-                    dist[nr][nc] = dist[r][c] +1 #bfs는 큐에 넣고 선 처리
-                    # print(nr,nc,dist[nr][nc])
-
-    bfs(0,0) #출발지가 하나이기 때문에 bfs를 한번만 호출
-  
+                    
+    dist = [ [-1]* m for _ in range(n)]
     # print(dist)
-
-    
-    if dist[n-1][m-1] == -1:
-        return -1
-    else: 
-        return dist[n-1][m-1]+1
+    bfs(0,0)
+    # print(dist)
+    return dist[n-1][m-1]
