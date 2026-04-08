776dfbfdd1f4229d2d1c555b9055701528ad9b15
Yu Jin <qldls0307@naver.com>
2026-04-08 15:58:04 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,8 +2,6 @@ def solution(array, commands):
     answer = []
     
     for i,j,k in commands:
-        temp = array[i-1:j]
-        temp.sort()
-        answer.append(temp[k-1])
-    
+        new = sorted(array[i-1:j])
+        answer.append(new[k-1])
     return answer
