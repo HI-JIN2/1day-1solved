3b212e7ee68c23c9acf13ae43ed4a2422d815a41
Yu Jin <qldls0307@naver.com>
2026-04-09 10:21:25 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+SELECT DISTINCT d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+FROM DEVELOPERS d
+JOIN SKILLCODES s
+    ON d.SKILL_CODE & s.CODE != 0  #포함되어 있음 
+WHERE s.CATEGORY = 'Front End'
+ORDER BY d.ID;
+                    
