08a3f67be1ea1baf109ed71671ad2098f4a66898
Yu Jin <jini1514@soongsil.ac.kr>
2026-04-09 10:09:32 +0900

    Refactor SQL query for purchased user ratio

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,17 +1,12 @@
--- 코드를 입력하세요
-
-
 select 
-# online_sale_id, u.user_id,  DATE_FORMAT(sales_date, "%Y-%m"), joined
-YEAR(SALES_DATE) as YEAR, MONTH(SALES_DATE) as MONTH, 
-# u.USER_ID,
-COUNT(distinct u.USER_ID) as PURCHASED_USERS
-, round(COUNT(distinct u.USER_ID)/(
-    select  count(u.user_id)
-    from USER_INFO u
-    where DATE_FORMAT(u.JOINED, "%Y") = "2021"
-),1) as PUCHASED_RATIO
-# select  count(u.user_id)
+    YEAR(SALES_DATE) as YEAR, 
+    MONTH(SALES_DATE) as MONTH, 
+    COUNT(distinct u.USER_ID) as PURCHASED_USERS, 
+    round(COUNT(distinct u.USER_ID)/(
+        select  count(u.user_id) 
+        from USER_INFO u
+        where DATE_FORMAT(u.JOINED, "%Y") = "2021" #분모는 2021가입이면 됨
+    ),1) as PUCHASED_RATIO
 from ONLINE_SALE o
 join USER_INFO u on u.USER_ID = o.USER_ID
 where DATE_FORMAT(u.JOINED, "%Y") = "2021"
