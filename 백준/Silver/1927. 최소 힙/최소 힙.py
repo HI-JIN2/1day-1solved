3f8d134082b1c19c3f63bde90dd2d0d249f3d9c8
Yu Jin <qldls0307@naver.com>
2026-04-15 15:21:19 +0900

    [Silver II] Title: 최소 힙, Time: 104 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,19 @@
+import heapq
+import sys
+
+
+input = sys.stdin.readline
+
+t = int(input())
+
+arr = [ ]
+
+for i in range(t):
+    x = int(input())
+    if x == 0:
+        if arr:
+            print(heapq.heappop(arr))
+        else:
+            print(0)
+    else:
+        heapq.heappush(arr,x)
